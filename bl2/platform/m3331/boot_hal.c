0d0322bf65bb5e3b52f4ba1c5cb23cdf64cef2d0
phchen9 <PHCHEN9@nuvoton.com>
2025-02-21 17:46:24 +0800

    New: Support M3331

---
+++ bl2/platform/m3331/boot_hal.c
@@ -0,0 +1,29 @@
+#include "NuMicro.h"
+#include "boot_hal.h"
+
+/* bootloader platform-specific hw initialization */
+int32_t boot_platform_init(void)
+{
+    SYS_UnlockReg();
+
+    /* Enable HXT clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Wait for HXT clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Enable UART0 module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Select UART0 module clock source as HIRC and UART0 module clock divider as 1 */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SET_UART0_RXD_PB12();
+    SET_UART0_TXD_PB13();
+
+    /* Init UART to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+
+    return 0;
+}
